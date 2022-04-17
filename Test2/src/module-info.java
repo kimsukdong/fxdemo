2185ad2a5fc3aad94e969b109eaabfefb04ceb01
sukdong-kim <73944038+sukdong-kim@users.noreply.github.com>
2022-04-17 21:16:09 +0900

    Demo for javafx

---
+++ Test2/src/module-info.java
@@ -0,0 +1,10 @@
+module Test2 {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.sql;
+	requires java.desktop;
+	requires javafx.graphics;
+	
+	opens phone to javafx.graphics, javafx.fxml,javafx.base;
+	opens phone.controller to javafx.graphics, javafx.fxml;
+}
